c56997abb559c46bddceffaf2919cafbfb94cf75
Ikuma Sugawara <admjz1411@gmail.com>
2020-07-21 18:32:18 +0900

    ToDo一覧にIDを追加+seedファイルに初期データを記述

--- database/seeds/TodosTableSeeder.php
+++ database/seeds/TodosTableSeeder.php
@@ -21,10 +21,22 @@ class TodosTableSeeder extends Seeder
                 'updated_at' => Carbon::create(2018, 1, 4),
             ],
             [
-                'user_id'    => '2',
+                'user_id'    => '1',
                 'title'      => 'レビューに向けて理解を深める',
                 'created_at' => Carbon::create(2018, 2, 1),
                 'updated_at' => Carbon::create(2018, 2, 5),
+            ],
+            [
+                'user_id'    => '1',
+                'title'      => '確認投稿！',
+                'created_at' => Carbon::create(2020, 2, 1),
+                'updated_at' => Carbon::create(2020, 2, 1),
+            ],
+            [
+                'user_id'    => '1',
+                'title'      => 'gitLesson用',
+                'created_at' => Carbon::create(2010, 2, 1),
+                'updated_at' => Carbon::create(2010, 2, 5),
             ],
         ]);
     }
